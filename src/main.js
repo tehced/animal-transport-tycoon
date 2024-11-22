2b0d58710af99918b583be686f051e71efd5af96
tehced <brettm.briggs@gmail.com>
2024-11-22 12:40:28 -0500

    basic main menu

--- src/main.js
+++ src/main.js
@@ -1,32 +1,59 @@
 /*
-    Animal Transport Tycoon
+    Little JS Animal Transport Tycoon
 
 */
 
 'use strict';
-
-// import statements
 import * as Objects from '/src/objects.js';
 
-if (!debug)
-{
-    showWatermark = false;
-    showSplashScreen = true;
+// showWatermark = false;
+// showSplashScreen = true;
+
+let levelSize;
+let cursor;
+let startGameButton;
+let settingsButton;
+
+const Scene = {
+    MainMenu: 0,
+    Game: 1,
+    Settings: 2,
 }
 
+let currentScreen = Scene.MainMenu;
 
 ///////////////////////////////////////////////////////////////////////////////
 function gameInit()
 {
     // called once after the engine starts up
     // setup the game
+
+    canvasFixedSize = vec2(1280,720); //720p Landscape
+    levelSize = vec2(1280,720);
+    cameraPos = levelSize.scale(0.5);
+    cameraScale = 16;
+    cursor = new Objects.Cursor(vec2(levelSize.x / 2, levelSize.y / 2));
+
+    if (currentScreen === Scene.MainMenu)
+    {
+        startGameButton = new Objects.StartButton(vec2(cameraPos.x, cameraPos.y - 2));
+        settingsButton = new Objects.SettingsButton(vec2(cameraPos.x, cameraPos.y - 12));
+    }
+    
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 function gameUpdate()
 {
     // called every frame at 60 frames per second
-    // handle input and update the game state
+    // // handle input and update the game state
+
+    cursor.pos = mousePos;
+
+    if (startGameButton.selected) {
+        startGame();
+        goToState(Scene.Game);
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -34,6 +61,7 @@ function gameUpdatePost()
 {
     // called after physics and objects are updated
     // setup camera and prepare for render
+    
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -41,16 +69,46 @@ function gameRender()
 {
     // called before objects are rendered
     // draw any background effects that appear behind objects
+    const font = new FontImage();
+
+    if (currentScreen === Scene.MainMenu) {
+        
+        font.drawText("Animal\nTransport\nTycoon", vec2(cameraPos.x, cameraPos.y + 20), 0.6, true);
+        
+        // drawTile(vec2(cameraPos.x, cameraPos.y + 16 + Math.sin(timeReal)), vec2(12), 1, tileSizeDefault, new Color(1,1,1,1), 0, 0, new Color(0,0,0,0), true);
+
+        return;
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 function gameRenderPost()
 {
     // called after objects are rendered
-    // draw effects or hud that appear above all objects
-
+    // // draw effects or hud that appear above all objects
+    
 }
 
-///////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+function goToState(scene)
+{
+    if (currentScreen !== scene)
+    {
+        currentScreen = scene;
+    }
+}
+
+function startGame()
+{
+    if (currentScreen === Scene.MainMenu)
+    {
+        startGameButton.destroy();
+        settingsButton.destroy();
+    }
+
+    
+}
+/////////////////////////////////////////////////////////////////////////////
+
 // Startup LittleJS Engine
 engineInit(gameInit, gameUpdate, gameUpdatePost, gameRender, gameRenderPost);
--- src/objects.js
+++ src/objects.js
@@ -1,3 +1,98 @@
+class Cursor extends EngineObject {
+    constructor(pos)
+    {
+        super(pos, vec2(0));
+        this.setCollision(1,1);
+
+        this.renderOrder = 20
+    }
+
+    update()
+    {
+        // this.pos.x = mousePos.x;
+        // this.pos.y = mousePos.y;
+
+        // this.pos.x = clamp(this.pos.x, this.size.x / 2, levelSize.x - this.size.x / 2);
+        // this.pos.y = clamp(this.pos.y, this.size.y / 2, levelSize.y - this.size.y / 2);
+    }
+}
+
+class Button extends EngineObject {
+    constructor(pos, size, text, number, color, backgroundColor)
+    {
+        super(pos, size);
+        this.text = text;
+        this.number = number;
+        this.color = color;
+        this.backgroundColor = backgroundColor;
+        this.selected = false;
+        this.font = new FontImage(undefined, vec2(8), vec2(0,1));
+        this.setCollision(1);
+    }
+
+    render()
+    {
+        if (this.selected) {
+            this.color = new Color(1,0,1);
+            this.backgroundColor = new Color(0.5,0,0.5);
+        } else {
+            this.color = new Color(1,0,0);
+            this.backgroundColor = new Color(0.5,0,0);
+        }
+
+        drawRect(vec2(this.pos.x, this.pos.y), this.size, this.backgroundColor, 0, false);
+        drawRect(vec2(this.pos.x, this.pos.y), vec2(this.size.x - 1, this.size.y - 1), this.color, 0, false);
+        this.font.drawText(this.text, vec2(this.pos.x, this.pos.y + 1.5), 0.2, true);
+        if (this.number > -1) {
+            this.font.drawText(this.number.toString(), vec2(this.pos.x + 5, this.pos.y - 1), 0.2, true);
+        }
+    }
+
+    collideWithObject(o)
+    {
+        if (isOverlapping(o.pos, o.size, this.pos, this.size) && isDown()) {
+            this.doSelect();
+        }
+        return false;
+    }
+
+    doSelect()
+    {
+        engineObjects.forEach((obj) => {
+            if (obj instanceof Button) {
+                obj.selected = false;
+            }
+        })
+        this.selected = true;
+    }
+}
+
+class StartButton extends Button {
+    constructor(pos, cursor) 
+    {
+        super(pos, vec2(18,8), "Start\nGame", -1, new Color(1,0,0), new Color(0.5,0,0));
+    }
+}
+
+class SettingsButton extends Button {
+    constructor(pos)
+    {
+        super(pos, vec2(18,8), "Settings", -1, new Color(1,0,0), new Color(0.5,0,0));
+    }
+}
+
+function isDown() {
+    return mouseIsDown(0)
+}
+
+function isClicked(o) {
+    return (o === cursor && mouseIsDown(0))
+}
+
 export {
-    
+
+    Cursor,
+    Button,
+    StartButton,
+    SettingsButton
 }
